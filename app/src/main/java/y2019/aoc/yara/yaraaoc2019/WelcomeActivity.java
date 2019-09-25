0f7f111f15b69de6ab6b4ff154377e3a9da4e2c4
yara2019 <yarakara9@gmail.com>
2019-09-25 11:11:00 +0300

    kjhk

--- app/src/main/java/y2019/aoc/yara/yaraaoc2019/WelcomeActivity.java
+++ app/src/main/java/y2019/aoc/yara/yaraaoc2019/WelcomeActivity.java
@@ -1,7 +1,10 @@
 package y2019.aoc.yara.yaraaoc2019;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class WelcomeActivity extends AppCompatActivity {
@@ -19,5 +22,26 @@ public class WelcomeActivity extends AppCompatActivity {
 
 
 
+
+
+    }
+
+    //presents the selected menu layout
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.my_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId() == R.id.more_info){
+
+        }else if(item.getItemId() == R.id.log_out){
+            Intent k = new Intent(this , LogInActivity.class);
+            startActivity(k);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
