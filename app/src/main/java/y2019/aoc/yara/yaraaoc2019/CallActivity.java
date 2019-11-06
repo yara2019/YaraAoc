9f71ce7107f29773b7aa0c939cb1dd80f346c9b2
yara2019 <yarakara9@gmail.com>
2019-11-06 11:07:25 +0200

    kjhk

--- app/src/main/java/y2019/aoc/yara/yaraaoc2019/CallActivity.java
+++ app/src/main/java/y2019/aoc/yara/yaraaoc2019/CallActivity.java
@@ -5,13 +5,15 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class CallActivity extends AppCompatActivity implements View.OnClickListener {
-    Button buttonCall , buttonBack;
+    Button buttonCall, buttonBack;
+    private static final int REQUEST_PHONE_CALL = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +29,13 @@ public class CallActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         if (v == buttonCall) {
             Intent i = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "0527564565"));
-            startActivity(i);
+            if (ContextCompat.checkSelfPermission(CallActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(CallActivity.this, new String[]{Manifest.permission.CALL_PHONE},REQUEST_PHONE_CALL);
+            }
+            else
+            {
+                startActivity(i);
+            }
 
         }
         if(v == buttonBack){
